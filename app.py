64b342d83b64d78eca7c8e8765346764d93472dc
Christian Scorpan <christianscorpan@gmail.com>
2024-06-08 16:36:27 +0200

    price filter v2

--- app.py
+++ app.py
@@ -31,35 +31,24 @@ def book_details(book_id):
     conn.close()
     return render_template('book_details.html', book=book)
 
+
 @app.route('/search')
 def search():
     query = request.args.get('query', '')
-    conn = get_db_connection()
-    books = conn.execute('SELECT id, title FROM books WHERE title LIKE ?', (f'%{query}%',)).fetchall()
-    conn.close()
-    return render_template('books.html', titles=books)
+    min_price = request.args.get('min_price', '0')
+    max_price = request.args.get('max_price', '0')
 
-@app.route('/search_by_price')
-def search_by_price():
-    price = request.args.get('price', '1')
-    
-
-    less_than_5 = re.compile(r'^\s*\$?([0-4](\.\d{1,2})?|5(\.0{1,2})?)\s*$')
-    between_5_and_10 = re.compile(r'^\s*\$?(5(\.\d{1,2})?|[6-9](\.\d{1,2})?|10(\.0{1,2})?)\s*$')
-    more_than_10 = re.compile(r'^\s*\$?(1[1-9](\.\d{1,2})?|[2-9]\d(\.\d{1,2})?|[1-9]\d{2,}(\.\d{1,2})?)\s*$')
+    if not min_price:
+        min_price = '0'
+    if not max_price:
+        max_price = '999999999'  # A large value to include all prices if max_price is not provided
 
     conn = get_db_connection()
-    books = conn.execute('SELECT id, title, price FROM books').fetchall()
+    books = conn.execute('SELECT id, title, price FROM books WHERE title LIKE ? AND price BETWEEN ? AND ?',
+                         (f'%{query}%', min_price, max_price)).fetchall()
     conn.close()
-    
-    if price == '1':
-        filtered_books = [book for book in books if less_than_5.match(str(book['price']))]
-    elif price == '2':
-        filtered_books = [book for book in books if between_5_and_10.match(str(book['price']))]
-    elif price == '3':
-        filtered_books = [book for book in books if more_than_10.match(str(book['price']))]
-    
-    return render_template('books.html', titles=[(book['id'], book['title']) for book in filtered_books])
+
+    return render_template('books.html', titles=[(book['id'], book['title']) for book in books])
 
 if __name__ == '__main__':
     app.run(debug=True)
