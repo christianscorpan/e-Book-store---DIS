b2e60e8411a68c52f1fc60db353b7984ec6a2a21
Christian Scorpan <christianscorpan@gmail.com>
2024-06-08 17:08:32 +0200

    added genre filter

--- app.py
+++ app.py
@@ -11,11 +11,19 @@ def get_db_connection():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    categories = conn.execute('SELECT DISTINCT category FROM books').fetchall()
+    conn.close()
+
+    return render_template('index.html', categories=[category['category'] for category in categories])
 
 @app.route('/store')
 def store():
-    return render_template('store.html')
+    conn = get_db_connection()
+    categories = conn.execute('SELECT DISTINCT category FROM books').fetchall()
+    conn.close()
+
+    return render_template('store.html', categories=[category['category'] for category in categories])
 
 @app.route('/books')
 def books_list():
@@ -37,6 +45,7 @@ def search():
     query = request.args.get('query', '')
     min_price = request.args.get('min_price', '0')
     max_price = request.args.get('max_price', '0')
+    selected_category = request.args.get('category', '')
 
     if not min_price:
         min_price = '0'
@@ -44,7 +53,8 @@ def search():
         max_price = '999999999'  # A large value to include all prices if max_price is not provided
 
     conn = get_db_connection()
-    books = conn.execute('SELECT id, title, price FROM books').fetchall()
+    books = conn.execute('SELECT id, title, price, category FROM books').fetchall()
+    categories = conn.execute('SELECT DISTINCT category FROM books').fetchall()
     conn.close()
 
     # Filter books based on search query using regex
@@ -53,7 +63,11 @@ def search():
     # Filter books based on price range
     filtered_books = [book for book in filtered_books if float(min_price) <= book['price'] <= float(max_price)]
 
-    return render_template('books.html', titles=[(book['id'], book['title']) for book in filtered_books])
+    # Filter books based on selected category
+    if selected_category:
+        filtered_books = [book for book in filtered_books if book['category'] == selected_category]
+
+        return render_template('books.html', titles=[(book['id'], book['title']) for book in filtered_books], categories=[category['category'] for category in categories])
 
 if __name__ == '__main__':
     app.run(debug=True)
