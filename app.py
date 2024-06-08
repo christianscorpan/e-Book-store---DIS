152df431bb425aeca4bace4ee9e1481731429cbd
Christian Scorpan <christianscorpan@gmail.com>
2024-06-08 17:32:07 +0200

    filter

--- app.py
+++ app.py
@@ -44,13 +44,8 @@ def book_details(book_id):
 def search():
     query = request.args.get('query', '')
     min_price = request.args.get('min_price', '0')
-    max_price = request.args.get('max_price', '0')
+    max_price = request.args.get('max_price', '999999999')
     selected_category = request.args.get('category', '')
-
-    if not min_price:
-        min_price = '0'
-    if not max_price:
-        max_price = '999999999'  # A large value to include all prices if max_price is not provided
 
     conn = get_db_connection()
     books = conn.execute('SELECT id, title, price, category FROM books').fetchall()
@@ -66,8 +61,10 @@ def search():
     # Filter books based on selected category
     if selected_category:
         filtered_books = [book for book in filtered_books if book['category'] == selected_category]
+    else:
+        selected_category = 'All Genres'
 
-        return render_template('books.html', titles=[(book['id'], book['title']) for book in filtered_books], categories=[category['category'] for category in categories])
+    return render_template('books.html', titles=[(book['id'], book['title']) for book in filtered_books], categories=[category['category'] for category in categories], selected_category=selected_category, min_price=min_price, max_price=max_price, query=query)
 
 if __name__ == '__main__':
     app.run(debug=True)
